9f0f958f8abc60795f744ccb69a644378a4e2b84
AlexieGalaxy <alexpenrose123@gmail.com>
2020-02-22 05:33:34 +0000

    Added plane to menu, transition base and fixed dialogueBox.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime;
+
+    public void LoadNextScene()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    // Co-routine
+    IEnumerator LoadLevel(int sceneIndex)
+    {
+        // Play animation
+        transition.SetTrigger("Start");
+
+        // Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        // Load Scene.
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
