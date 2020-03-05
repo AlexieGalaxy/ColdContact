7e70dea750e399f4f0e6081fa49455957b1deda3
AlexieGalaxy <alexpenrose123@gmail.com>
2020-03-05 04:10:20 +0000

    Adding functionality to the main menu.

---
+++ Assets/Scripts/Openable.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Openable : MonoBehaviour
+{
+    public Rigidbody2D player;
+    public GameObject prompt;
+    public Dialogue dialogue;
+    private BoxCollider2D trigger;
+    private Vector3 promptPos;
+
+    private bool hasOpened = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        promptPos = new Vector3(transform.position.x, transform.position.y + 2.0f, transform.position.z);
+        // Chest sprite
+        trigger = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!hasOpened)
+        {
+            prompt.transform.position = promptPos;
+            prompt.SetActive(true);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown("f") && !hasOpened)
+        {
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+            hasOpened = true;
+            prompt.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        prompt.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer masterMixer, musicMixer, voiceMixer;
+    public TMPro.TMP_Dropdown resolutionDropdown;
+
+    public TMPro.TMP_Text masterValue, musicValue, voiceValue;
+    public Slider masterSlider, musicSlider, voiceSlider;
+
+    private Resolution[] userResolutions;
+
+    private void Start()
+    {
+        userResolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < userResolutions.Length; i++)
+        {
+            string option = userResolutions[i].width + " * " + userResolutions[i].height;
+            options.Add(option);
+
+            if (userResolutions[i].width == Screen.currentResolution.width &&
+                    userResolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void Update()
+    {
+        masterValue.text = Mathf.Ceil(masterSlider.value * 100f) + "%";
+        musicValue.text = Mathf.Ceil(musicSlider.value * 100f) + "%";
+        voiceValue.text = Mathf.Ceil(voiceSlider.value * 100f) + "%";
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = userResolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        masterMixer.SetFloat("masterVolume", volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicMixer.SetFloat("musicVolume", volume);
+    }
+
+    public void SetVoiceVolume(float volume)
+    {
+        voiceMixer.SetFloat("voiceVolume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
